13f50862a2272e99cffeab58d89ac1d64c4e82c4
unknown <mvsamaniego1@utpl.edu.ec>
2023-11-22 16:51:52 -0500

    Subir cambios

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo03.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo03.java
@@ -36,7 +36,7 @@ public class Ejemplo03 {
             valor_ingresado = entrada.nextInt();
             suma = suma + valor_ingresado;
             System.out.printf("Valor ingresado %d\n", valor_ingresado);
-            //contador = contador + 1;//
+            contador = contador + 1;
         }
 
         System.out.printf("La suma final es %d\n", suma);
